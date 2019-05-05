0290c6fc432f149597d7614f4aac6d4b9f449c33
Jin ZHANG <jz.zhangjin@gmail.com>
2019-05-05 16:57:54 +0800

    jsx and vdom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
-// import l10n.js first
-import 'kaios-gaia-l10n';
-import './index.css';
+const React = {
+  createElement
+};
 
-window.addEventListener('load', () => {
-  const anchor = document.getElementById('app-link');
-  anchor.focus();
-});
+const ReactDOM = {
+  render: (vnode, container) => {
+    container.innerHTML = '';
+    return render(vnode, container);
+  }
+};
+
+function createElement(tag, attrs, ...children) {
+  return {
+    tag,
+    attrs,
+    children
+  }
+}
+
+function render(vnode, container) {
+  if (typeof vnode === 'string') {
+    let textNode = document.createTextNode(vnode);
+    return container.appendChild(textNode);
+  }
+
+  const dom = document.createElement(vnode.tag);
+  if (vnode.attrs) {
+    Object.keys(vnode.attrs).forEach(key => {
+      const value = vnode.attrs[key];
+      setAttribute(dom, key, value);
+    });
+  }
+
+  vnode.children.forEach(child => render(child, dom));
+  return container.appendChild(dom);
+}
+
+function setAttribute(dom, name, value) {
+  if (name === 'className') name = 'class';
+
+  if (/on\w+/.test(name)) {
+    name = name.toLowerCase();
+    dom[name] = value || '';
+  } else if (name === 'style') {
+    if (!value || typeof value === 'string') {
+      dom.style.cssText = value || '';
+    } else if (value && typeof value === 'object') {
+      for (let name in value) {
+        dom.style[name] = typeof value[name] === 'number' ? value[name] + 'px' : value[name];
+      }
+    }
+  } else {
+    if (name in dom) {
+      dom[name] = value || '';
+    }
+    if (value) {
+      dom.setAttribute(name, value);
+    } else {
+      dom.removeAttribute(name, value);
+    }
+  }
+}
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  // console.log(element);
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
